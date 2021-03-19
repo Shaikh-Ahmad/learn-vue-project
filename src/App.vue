c16b8c70198810165a5c48abd35ed31da1dfe0cd
Shaikh-Ahmad <xheikh.ahmed@gmail.com>
2021-03-19 04:42:34 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,62 @@
 <template>
   <div class="container">
     <Header title="Task Traker"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
   },
   data(){
     return{
       tasks : [ ]
     }
+  },
+  methods:{
+    deleteTask(id){
+      if(confirm('Are you sure you want to delete')){
+      // console.log('task' , id)
+      this.tasks = this.tasks.filter( (task) => task.id !== id )
+      }
+    },
+  },
+  
+  created(){
+    this.tasks = [
+      {
+        id: 1,
+        text: 'meeting',
+        day: '2 feb 2021',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'doc appoitment',
+        day: '2 feb 2021',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'school meeting',
+        day: '20 april 2021',
+        reminder: true,
+      },
+      {
+        id: 4,
+        text: 'client meeting',
+        day: '5 feb 2021',
+        reminder: false,
+      },
+     
+    ]
   }
 }
 </script>
@@ -25,13 +66,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin: 60px;
 }
 .container{
+  
   width: 70%;
-  margin: 60px;
   padding: 30px;
   border: 1px solid grey;
 }
---
+++ src/components/Task.vue
@@ -0,0 +1,41 @@
+<template>
+    <div  :class="[task.reminder ? 'reminder' : '' , 'task']">
+        <h3>{{task.text}}
+            <i @click="onDelete(task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+    methods:{
+        onDelete(id){
+            this.$emit('delete-task', id)
+            // console.log(id);
+        },
+    },
+}
+</script>
+
+<style>
+.fas{
+    float: right;
+    color: red;
+    padding: 15px;
+}
+.task{
+    background-color: rgb(231, 231, 231);
+    margin: 4px;
+    padding: 2px;
+    padding-left : 15px;
+}
+.task.reminder {
+    border-left: 8px solid green;
+}
+</style>
